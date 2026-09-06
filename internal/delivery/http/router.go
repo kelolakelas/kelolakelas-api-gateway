c093ae1717c371238ebf132df70520c4c0aaf6e8
Ahmad Farid Imam Zamani <skinel123@gmail.com>
2026-09-06 20:52:10 +0700

    chore: save gateway changes

--- internal/delivery/http/router.go
+++ internal/delivery/http/router.go
@@ -128,6 +128,7 @@ func NewRouterWithConfig(proxyHandler *handler.ProxyHandler, jwtSecret, appURL s
 			// Enrollment Management routes
 			protected.GET("/enrollments", proxyHandler.ProxyToAcademicService())
 			protected.GET("/enrollments/:id", proxyHandler.ProxyToAcademicService())
+			protected.PATCH("/enrollments/:id/schedule", proxyHandler.ProxyToAcademicService())
 			protected.POST("/tenants/:tenant_id/enrollments", proxyHandler.ProxyToAcademicService())
 			protected.POST("/catalog/classes/:class_id/enrollments", proxyHandler.ProxyToAcademicService())
 
